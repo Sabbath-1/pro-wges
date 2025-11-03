dc893eca17cb31d2ccc198d1bc69f5c1bd54968c
Elvin Quamina <quaminaelvin@gmail.com>
2025-11-03 20:12:26 +0000

    removed node modules

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('A user just a user');
+    res.render('index');
 });
 
 router.get('/new', (req, res) => {
